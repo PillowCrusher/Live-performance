3eb1dbbf0da5511e9e6fa040bd407e610c490bae
Jeroen Peters <jeroen.peters1@hotmail.nl>
2016-01-20 09:41:43 +0100

    commiting

---
+++ Applicatie/Live_performance/live_performance/live_performance/Data Layer/DatabaseManager.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using Oracle.ManagedDataAccess.Client;
+
+
+namespace live_performance.Data_Layer
+{
+    public class DatabaseManager
+    {
+            private static OracleConnection _connection;
+            public static OracleConnection Connection
+            {
+                get
+                {
+                    try
+                    {
+                        _connection = new OracleConnection("Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=fhictora01.fhict.local)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=fhictora))); User ID=dbi318334; PASSWORD = cQQBGJNsvk;");
+                        _connection.Open();
+                        return _connection;
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(Environment.NewLine + ex.Message + Environment.NewLine);
+                    }
+                    return null;
+                }
+            }
+
+            public static DataTable ExecuteReadQuery(string sqlquery, OracleParameter[] parameters)
+            {
+                try
+                {
+                    using (Connection)
+                    using (var command = new OracleCommand(sqlquery, _connection))
+                    {
+                        if (parameters != null)
+                        {
+                            command.Parameters.AddRange(parameters);
+                        }
+                        var dt = new DataTable();
+                        using (OracleDataReader reader = command.ExecuteReader())
+                        {
+                            dt.Load(reader);
+                        }
+
+                        return dt;
+                    }
+                }
+                catch (OracleException oe)
+                {
+                    Debug.WriteLine(oe.Message);
+                    return null;
+                }
+            }
+
+            public static void ExecuteInsertQuery(string sqlquery, OracleParameter[] parameters)
+            {
+                OracleConnection connection = Connection;
+                OracleTransaction transaction = connection.BeginTransaction();
+                OracleCommand command = new OracleCommand(sqlquery, connection);
+
+                if (parameters != null)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+
+                try
+                {
+                    command.ExecuteNonQuery();
+                    transaction.Commit();
+                }
+                catch (OracleException oe)
+                {
+                    Debug.WriteLine(Environment.NewLine + oe.Message + Environment.NewLine);
+                }
+                connection.Close();
+            }
+
+            public static void ExecuteDeleteQuery(string sqlquery, OracleParameter[] parameters)
+            {
+                using (Connection)
+                using (OracleTransaction ot = Connection.BeginTransaction())
+                {
+                    OracleCommand command = new OracleCommand(sqlquery, _connection);
+                    if (parameters != null)
+                    {
+                        command.Parameters.AddRange(parameters);
+                    }
+
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                        ot.Commit();
+                    }
+                    catch (OracleException oe)
+                    {
+                        Debug.WriteLine(Environment.NewLine + oe.Message + Environment.NewLine);
+                    }
+                }
+            }
+
+            public static bool CheckConnection()
+            {
+                try
+                {
+                    OracleConnection con = Connection;
+                    con.Close();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+    }
---
+++ Applicatie/Live_performance/live_performance/live_performance/WebForm1.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Live_Performance.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="live_performance.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Test" runat="server">
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+     <asp:Button ID="Button1" runat="server" Text="Button" CssClass="btn-primary left" OnClick="Button1_OnClick"/>
+</asp:Content>
